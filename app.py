6c1a957bcdb42becbf09cceddfc24220768e73af
bayembaye <bayembayebiteye22@gmail.com>
2025-07-25 15:43:33 +0000

    final commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+
+# Chargement
+model = joblib.load("model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+st.title("Classification de texte")
+
+user_input = st.text_area("Entrez votre texte ici :")
+
+if st.button("Prédire"):
+    if user_input.strip() == "":
+        st.warning("Veuillez entrer un texte.")
+    else:
+        input_vector = vectorizer.transform([user_input])
+        prediction = model.predict(input_vector)
+        st.success(f"Classe prédite : {prediction[0]}")
